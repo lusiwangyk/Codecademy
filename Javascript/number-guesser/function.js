c619db2500b810803f74a74159e98cd29839679e
Lusi Wang <lusiwangyk@gmail.com>
2020-05-18 09:39:37 -0400

    update files

--- Javascript/number-guesser/function.js
+++ Javascript/number-guesser/function.js
@@ -1,12 +1,27 @@
-let round = 1;
+var round = 1;
+var target = 0;
+var computerGuess = 0;
+target = Math.floor(Math.random() * 10);
+computerGuess = Math.floor(Math.random() * 10);
+console.log(target);
+console.log(computerGuess);
+
 document.getElementById('round').innerHTML = "Round " + round;
 
-let target = 0;
-target = Math.floor(Math.random() * 10);
-console.log(target);
-
+document.getElementById('next').addEventListener('click',function(){
+    round++;
+    document.getElementById('round').innerHTML = "Round " + round;
+    target = Math.floor(Math.random() * 10);
+    computerGuess = Math.floor(Math.random() * 10);
+    console.log(target);
+    console.log(computerGuess);
+    document.getElementById('number').innerHTML = '0';
+})
 
 var number = 0;
+var humanScore = 0;
+var computerScore = 0;
+
 document.getElementById('plus').addEventListener('click',function(){
     number++;
     if (number > 9){
@@ -14,7 +29,9 @@ document.getElementById('plus').addEventListener('click',function(){
         number = 0;
     }
     document.getElementById('number').innerHTML = number;
+    
 })
+
 document.getElementById('minus').addEventListener('click',function(){
     number--;
     if (number <0){
@@ -25,6 +42,19 @@ document.getElementById('minus').addEventListener('click',function(){
 })
 
 document.getElementById('guesser').addEventListener('click',function(){
-    if ()
+    if (number == target && computerGuess !== target){
+        humanScore++;
+        alert('You won one score.')
+        document.getElementById('humanscore').innerHTML = "score: " + humanScore;
+    }
+    else if (computerGuess == target && number !== target){
+        computerScore++;
+        alert('Computer won.')
+        document.getElementById('compscore').innerHTML = "score: " + computerScore;
+    }
+    else if (computerGuess == number) {
+        alert('Please guess again.')
+    }
 })
+
 
